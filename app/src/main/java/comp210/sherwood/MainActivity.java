c690b6b8214d04c9fa5a44de95e79bb9df96b814
sMattSherwood <matthew.sherwood@student.sl.on.ca>
2023-02-08 14:34:20 -0500

    when app opens all cells are blank, and users cannot over write the original selection. also added documentaion

--- app/src/main/java/comp210/sherwood/MainActivity.java
+++ app/src/main/java/comp210/sherwood/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
     /*
@@ -16,12 +18,13 @@ public class MainActivity extends AppCompatActivity {
     boolean playerX = true;
     int col = 3;
     int row = 3;
-    square[][] tttBoard; // this will hold the boards content in a gid format
-
+    square[][] ttt = new square[row][col]; // this will hold the boards content in a gid format
+    TableLayout board;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        board = (TableLayout)findViewById(R.id.tttBoard);
         setTags();
     }
 
@@ -31,10 +34,13 @@ public class MainActivity extends AppCompatActivity {
         *  3 X 3 grid
         * for each cell it will initialize with the blank image*/
         for(int x = 0; x < row; x++ ) {
+            TableRow tr = (TableRow) board.getChildAt(x); // will target the cell on the row
             for (int y = 0; y < col; y++) {
-                tttBoard[x][y] = new square(x,y);
-                tttBoard[x][y].setReasourceId(R.drawable.blank);
-
+                ImageView iv = (ImageView) tr.getChildAt(y); // will target the cell on the column
+                ttt[x][y] = new square(x,y);
+                ttt[x][y].setReasourceId(R.drawable.blank); // sets cell blank
+                iv.setTag(ttt[x][y]); // this will set the id of the col row cell
+                iv.setImageResource((R.drawable.blank)); // auto blank
             }
         }
 
@@ -46,11 +52,26 @@ public class MainActivity extends AppCompatActivity {
     * */
     public void changeImage(View view){
         ImageView img = (ImageView) view;
-        if(playerX)
-            img.setImageResource(R.drawable.x); // if player one clicks it will display x
-        else
-            img.setImageResource(R.drawable.o);
+        square current = (square)img.getTag();
+        /* if current cell id is equal to id for
+           blank let player use x or o
+           depending if playerX is true then player places down X
+           when playerX is false place down O
+         */
 
-        playerX = !playerX; // makes playerx false
+        if(current.reasourceId == R.drawable.blank)
+        {
+            if(playerX) {
+                img.setImageResource(R.drawable.x); // if player clicks cell display X
+                current.setReasourceId(R.drawable.x); // cell id will match X
+            }
+            else {
+                img.setImageResource(R.drawable.o); // if player clicks cell display O
+                current.setReasourceId(R.drawable.o); // cells id will match O
+            }
+
+            playerX = !playerX; // turns playerX value false
+        }
+
     }
 }
